397b2eff533ab5bee48074e8d22c52b1c0caf4f5
Argemiro Leite <aipleite@gmail.com>
2022-07-17 17:49:17 -0300

    Sale listing

--- frontend/src/components/Salescard/index.tsx
+++ frontend/src/components/Salescard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from '../NotificationButton';
 import './style.css';
 
@@ -13,10 +15,12 @@ function Salescard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
+        axios.get(`${BASE_URL}/sales`)
             .then(response => {
-                console.log(response.data);
+                setSales(response.data.content);
             })
 
     }, []);
@@ -57,49 +61,27 @@ function Salescard() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td className="dsmeta-showcolumn-higher-576px">08/07/2022</td>
-                            <td className="dsmeta-showcolumn-higher-992px">#341</td>
-                            <td>Anakin</td>
-                            <td className="dsmeta-showcolumn-higher-992px">15</td>
-                            <td className="dsmeta-showcolumn-higher-992px">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="dsmeta-showcolumn-higher-576px">08/07/2022</td>
-                            <td className="dsmeta-showcolumn-higher-992px">#254</td>
-                            <td>Eliezer</td>
-                            <td className="dsmeta-showcolumn-higher-992px">12</td>
-                            <td className="dsmeta-showcolumn-higher-992px">9</td>
-                            <td>R$ 63.800,00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
+                        {sales.map(sale => {
+                            return (
+                                <tr key ={sale.id}>
+                                    <td className="dsmeta-showcolumn-higher-576px">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td className="dsmeta-showcolumn-higher-992px">{sale.id}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="dsmeta-showcolumn-higher-992px">{sale.visited}</td>
+                                    <td className="dsmeta-showcolumn-higher-992px">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
 
-                                    <NotificationButton />
+                            )
+                        })
 
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="dsmeta-showcolumn-higher-576px">08/07/2022</td>
-                            <td className="dsmeta-showcolumn-higher-992px">#003</td>
-                            <td>Jessica</td>
-                            <td className="dsmeta-showcolumn-higher-992px">21</td>
-                            <td className="dsmeta-showcolumn-higher-992px">18</td>
-                            <td>R$ 49.890,00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
+                        }
 
-                                    <NotificationButton />
-
-                                </div>
-                            </td>
-                        </tr>
 
                     </tbody>
 
